27d2a2dc0aecd01d3d7bf86020117880a5ccaada
Yu Zhang <44559405+flameszhang@users.noreply.github.com>
2019-08-23 15:04:24 +0800

    submit

---
+++ ML_algorithms/adaboost.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
+
+#http://sklearn.lzjqsdd.com/modules/tree.html
+class Classifier():
+
+	def __init__(self):
+		self.clf = None
+		pass
+
+	def fit(self,X,Y):
+		self.clf = self.clf.fit(X, Y)
+
+	def predict(self,X):
+		return self.clf.predict(X)
+
+	def save(self,filename):
+		joblib.dump(self.clf, "model/%s"%(filename))
+
+	def load(self,filename):
+		try:
+			self.clf = joblib.load("model/%s"%(filename))
+		except BaseException:
+			self.clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),algorithm="SAMME",n_estimators=200)
+			return False
+		else:
+			return True
+
+if __name__ == "__main__":
+	filename = "a.clf"
+	clf = Classifier()
+	if not clf.load(filename):
+		X = [[0, 0], [1, 1]]
+		Y = [0, 1]
+		clf.fit(X,Y)
+		print(clf.predict([2,2]))
+		clf.save(filename)
+	else:
+		print("Model Loading Success")
+		print(clf.predict([2,2]))
+		print(clf.predict([0.2,0.2]))
+		print(clf.predict([0.55,0.55]))
+		print(clf.predict([0.5,0.5]))
